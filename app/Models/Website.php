e56ff20131eb75c676cb19af71affd5089d4e9dc
Dickens-Odera <dickensodera9@gmail.com>
2021-11-12 14:42:54 +0300

    Feat: Website - subscriber relationship

--- app/Models/Website.php
+++ app/Models/Website.php
@@ -4,16 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Website extends Model
 {
     use HasFactory, SoftDeletes;
 
+    /**
+     * @var string
+     */
     protected $table = 'websites';
 
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'domain'
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function subscribers(): HasMany
+    {
+        return $this->hasMany(Subscriber::class,'id','website_id');
+    }
 }
